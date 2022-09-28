19e768c5d3350b791e66638ea5f59d06394bac53
tanayazurange2805 <zurangetanaya57@gmail.com>
2022-09-28 13:36:47 +0530

    initial commit

---
+++ src/main/java/com/mahindra/idbi/banking/BankingDomainApplication.java
@@ -0,0 +1,13 @@
+package com.mahindra.idbi.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingDomainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahindra/idbi/banking/BankingDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahindra.idbi.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
